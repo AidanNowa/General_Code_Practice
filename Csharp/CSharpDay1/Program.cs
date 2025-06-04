8622d82e7e175e36bd46d7bc40cad29a61df0161
AidanNowa <aidannow@bu.edu>
2025-06-04 00:22:29 -0400

    setting up CSharp program

---
+++ Csharp/CSharpDay1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 7;
+        if (n % 2 == 0)
+        {
+            Console.WriteLine("Even");
+        }
+        else
+        {
+            Console.WriteLine("Odd");
+        }
+    }
+}
